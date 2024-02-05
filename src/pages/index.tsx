fa5c04c072b58dee7ac71ebfe1060ced6cba1ce9
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-02-06 04:58:28 +1100

    removed sticker label

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,11 +23,8 @@ export default function Home() {
     const [tilt, setTilt] = useState({ x: 0, y: 0 })
     const containerRef = useRef<HTMLDivElement | null>(null)
     const tableRef = useRef<HTMLDivElement | null>(null)
-    const [isImageVisible, setIsImageVisible] = useState(false)
     const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 })
-    const [hoveredImageUrl, setHoveredImageUrl] = useState('')
-    const imageRef = useRef<HTMLImageElement>(null)
-
+    
     const handleMouseMove = (e: MouseEvent) => {
         if (containerRef.current) {
             const { left, top, width, height } =
@@ -65,19 +62,6 @@ export default function Home() {
         }
     }, [])
 
-    const handleMouseEnter = (keyboard: KeyboardItem) => {
-        if (keyboard.date.toLowerCase() !== 'tbd') {
-            // remove whitespace from name
-            const name = keyboard.name.replace(/\s/g, '')
-            setHoveredImageUrl(name)
-            setIsImageVisible(true)
-        }
-    }
-
-    const handleMouseLeave = () => {
-        setIsImageVisible(false)
-    }
-
     return (
         <main
             className="overflow-hidden flex min-h-screen flex-col flex-inline bg-main max-w-full relative"
@@ -98,14 +82,14 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h1
+            {/* <h1
                 className={`absolute top-0 text-main text-xs md:text-sm p-1 w-44 md:w-52 text-center font-bold bg-accent whitespace-nowrap z-10 sticky ${orbitron.className}`}
             >
                 INDUSTRIAL GALLERY
-            </h1>
+            </h1> */}
 
             <div
-                className="select-none flex-grow flex items-center justify-center top-0 absolute w-full h-full"
+                className="select-none flex-grow flex items-center justify-center top-0 relative w-screen h-screen"
                 ref={containerRef}
             >
                 <img
@@ -120,7 +104,7 @@ export default function Home() {
                     }}
                 />
 
-                {/* <div className="absolute bottom-4 w-full flex justify-center mb-4 stroke-2">
+                <div className="absolute bottom-4 w-full flex justify-center mb-4 stroke-2">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="self-center stroke-accent"
@@ -141,15 +125,14 @@ export default function Home() {
                         <path d="M16 15l-4 4"></path>
                         <path d="M8 15l4 4"></path>
                     </svg>
-                </div> */}
+                </div>
             </div>
 
-            {/* <div className="max-w-full mt-8 min-h-[30vh] z-10">
-                <div className="uppercase z-0 w-full border-collapse divide-y-[1px] divide-main1/50 text-xs sm:text-sm md:text-base lg:text-lg select-none">
-                    <div className="text-accent text-left">
+            <div className="max-w-full mt-8 min-h-[30vh] z-10">
+                <div className="text-accent text-left border-y border-main1 uppercase">
                         <div className="flex flex-row">
-                            <div className="px-4 py-2 w-[20%]">Date</div>
-                            <div className="px-4 py-2 w-[30%] md:w-[50%]">
+                            <div className="px-4 py-2 w-[20%] border-r border-main1">Date</div>
+                            <div className="px-4 py-2 w-[30%] md:w-[50%] border-r border-main1">
                                 Name
                             </div>
                             <div className="px-4 py-2 w-[50%] md:w-[30%]">
@@ -157,16 +140,17 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    {keyboards.map((keyboard) => (
+                <div className="z-0 w-full border-collapse text-xs sm:text-sm md:text-base lg:text-lg select-none">
+                    
+                    {keyboards.map((keyboard, index) => (
                         <div
                             key={keyboard.name}
                             className={`text-left hover:bg-main1/20 z-10 ${
-                                keyboard.date === 'tbd'
+                                index % 2 === 0 ? 'bg-main1/10' : ''} ${
+                                keyboard.date === 'TBD'
                                     ? 'text-slate-500 disabled'
                                     : 'hover:text-accent'
                             }`}
-                            onMouseEnter={() => handleMouseEnter(keyboard)}
-                            onMouseLeave={handleMouseLeave}
                         >
                             <Link
                                 href={`/keyboards/${keyboard.name.replace(
@@ -174,7 +158,7 @@ export default function Home() {
                                     ''
                                 )}`}
                                 className={`${
-                                    keyboard.date === 'tbd'
+                                    keyboard.date === 'TBD'
                                         ? 'pointer-events-none'
                                         : ''
                                 }`}
@@ -193,20 +177,8 @@ export default function Home() {
                             </Link>
                         </div>
                     ))}
-                    <img
-                        ref={imageRef}
-                        src={`/data/${hoveredImageUrl}/front.jpg`}
-                        className={`fixed transform -translate-x-1/2 -translate-y-1/2 max-w-[30vw] max-h-[30vh] transition-opacity duration-500 ease-in-out ${
-                            isImageVisible ? 'opacity-100' : 'opacity-0'
-                        } pointer-events-none -z-10`}
-                        style={{
-                            left: cursorPosition.x,
-                            top: cursorPosition.y,
-                        }}
-                        alt="Hovered"
-                    />
                 </div>
-            </div> */}
+            </div>
         </main>
     )
 }
